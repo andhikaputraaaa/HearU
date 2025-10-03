39994bcce6a843946d75c927cdb390612bdb830c
ekwahh31 <eka.wicaksono@mhs.unsoed.ac.id>
2025-10-03 15:46:17 +0700

    Tampilan Page Posting

--- app/src/main/java/com/innovape/hearuapp/PostingActivity.kt
+++ app/src/main/java/com/innovape/hearuapp/PostingActivity.kt
@@ -1,19 +1,18 @@
 package com.innovape.hearuapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.innovape.hearuapp.databinding.ActivityHomeBinding
+import com.innovape.hearuapp.databinding.ActivityPostingBinding
 
-class HomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
+class PostingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPostingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityPostingBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
