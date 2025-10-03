d934618be30d73a2de33ef96bd4ab605a24160df
estehangat <daiva1paundra@gmail.com>
2025-10-03 17:40:55 +0700

    navbar

--- app/src/main/java/com/innovape/hearuapp/HomeActivity.kt
+++ app/src/main/java/com/innovape/hearuapp/HomeActivity.kt
@@ -8,12 +8,28 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.innovape.hearuapp.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
+class HomeActivity : AppCompatActivity(), Navbar.OnNavigationClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val bottomNavFragment = supportFragmentManager.findFragmentById(R.id.bottom_nav_container) as? Navbar
+        bottomNavFragment?.setOnNavigationClickListener(this)
+    }
+
+    override fun onHomeClick() {
+        val bottomNavFragment = supportFragmentManager.findFragmentById(R.id.bottom_nav_container) as? Navbar
+        bottomNavFragment?.setActiveItem(0) // 0 is the position for home
+    }
+
+    override fun onEditClick() {
+//        val intent = Intent(this, EditActivity::class.java)
+//        startActivity(intent)
+    }
+
+    override fun onProfileClick() {
+//        val intent = Intent(this, ProfileActivity::class.java)
+//        startActivity(intent)
     }
 }
